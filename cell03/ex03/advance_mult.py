2e963c2c48aba22fec90bc9588f318465d222cee
EurotheGong <25007@tun.ac.th>
2025-03-18 16:54:48 +0700

    cell00-03

---
+++ cell03/ex03/advance_mult.py
@@ -0,0 +1,11 @@
+multiplier = 0
+while multiplier <= 10:
+    print(f"Table de {multiplier}: ", end = "")
+
+    current_number = 0
+    while current_number <= 10:
+        print(multiplier * current_number, end = " ")
+        current_number += 1
+    
+    print()
+    multiplier += 1
